fe57adc18a77d4bbc3adb8e5aa0c236d56b88635
youngkidwarrior <ginelliunit@gmail.com>
2020-09-14 00:31:10 -0700

    module-errors

--- packages/app/metro.config.js
+++ packages/app/metro.config.js
@@ -6,11 +6,14 @@
  */
 const path = require('path');
 
+const reactNativePath = require.resolve('react-native');
+const reactNativeFolder = `${
+    reactNativePath.split('node_modules/react-native/')[0]
+}node_modules/react-native/`;
+
 module.exports = {
-    resolver: {
-        extraNodeModules: require('node-libs-browser')
-    },
     transformer: {
+        publicPath: '/assets/dark/magic',
         getTransformOptions: async () => ({
             transform: {
                 experimentalImportSupport: false,
@@ -18,5 +21,28 @@ module.exports = {
             }
         })
     },
-    watchFolders: [path.resolve(__dirname, '../../node_modules')]
+    resolver: {
+        extraNodeModules: require('node-libs-browser'),
+        blacklistRE: new RegExp(
+            `^((?!${reactNativeFolder.replace(
+                '/',
+                '\\/'
+            )}).)*\\/node_modules\\/react-native\\/.*$`
+        )
+    },
+    server: {
+        enhanceMiddleware: (middleware) => {
+            return (req, res, next) => {
+                if (req.url.startsWith('/assets/dark/magic')) {
+                    req.url = req.url.replace('/assets/dark/magic', '/assets');
+                } else if (req.url.startsWith('/assets/dark')) {
+                    req.url = req.url.replace('/assets/dark', '/assets/..');
+                } else if (req.url.startsWith('/assets')) {
+                    req.url = req.url.replace('/assets', '/assets/../..');
+                }
+                return middleware(req, res, next);
+            };
+        }
+    },
+    projectRoot: path.resolve(__dirname, '../../')
 };
